5659788b9ae7869e7dcaad82f7ee37db2f7a4b71
Josh Black <joshblack@tigger.local>
2017-08-17 12:58:51 -0600

    final rubocop

--- challenge.rb
+++ challenge.rb
@@ -1,4 +1,4 @@
 require 'top_secret'
 
 chevy_url = 'http://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page1/?filter=ALL_REVIEWS#link'
-puts  TopSecret::Scrape.positive(chevy_url, 5, 3)
+puts TopSecret::Scrape.positive(chevy_url, 5, 3)
