547ae449468fb4b3ce92814856bed68244c1bc17
Josh Black <joshblack@tigger.local>
2017-08-17 11:59:36 -0600

    added all test cases

--- lib/top_secret.rb
+++ lib/top_secret.rb
@@ -1,6 +1,7 @@
 require "top_secret/version"
 require 'rest-client'
 require 'nokogiri'
+require 'date'
 require 'pry'
 
 module TopSecret
@@ -10,58 +11,69 @@ module TopSecret
     def self.positive(url,pages,limit_to_return)
       reviews = []
       counter = 1
-
+      # replaces url and calls extract on correct number of pages starting with the first page.
       while counter <= pages
         url = url.sub("page" + (counter-1).to_s, "page" + counter.to_s )
         reviews.concat extract(url)
         counter = counter + 1
       end
-      return reviews
+      # sorts reviews based on teh overall score and then the date.
+      reviews.sort { |a, b| [a['overall'], Date.parse(a['date'])] <=> [b['overall'], Date.parse(b['date'])] }
+      return reviews.first(limit_to_return)
     end
 
     def self.extract(url)
       reviews = []
 
-      #use rest-client to get the page object
+      # use rest-client to get the page object
       page = RestClient.get(url)
 
-      #convert the page object into a nokogiri object to be parsed. The review-wrapper css class is used to identify reviews.
+      # convert the page object into a nokogiri object to be parsed. The review-wrapper css class is used to identify reviews.
       noko_page = Nokogiri::HTML(page).css('.review-entry')
 
-      #map through each user_review
+      # map through each user_review
       noko_page.map do |user_review|
         review_to_add = Hash.new
 
+        # the date and overall are located in an earlier div than the rest of the info locates in the review-wrapper container
         date_and_overall = user_review.css('.review-date').css('div')
         review_to_add["date"] = date_and_overall.css('.italic').text.strip
         review_to_add["overall"] = rating(date_and_overall.css('.dealership-rating').css('div')[1])
 
+        # the rest of the review
         review_wrapper = user_review.css('.review-wrapper').css('div')
 
+        # title and name
         review_title = review_wrapper[1]
         review_to_add["title"] = review_title.css('h3').text.strip
         review_to_add["name"] = review_title.css('span').text.strip
 
+        # the main text of the review
         review_body = review_wrapper[2]
         review_to_add["body"] = review_body.css('p').text.strip
 
+        # the specific ratings
         review_ratings = review_wrapper.css('.review-ratings-all').css('.table').css('.tr')
 
-
+        # map through specific review score and call rating to fetch correct score.
         review_ratings.map do |specific_rating|
           specific_score = rating(specific_rating.css('div')[1])
           review_to_add[specific_rating.css('.bold').text] = specific_score
         end
 
+        # component containing employees
         review_employees = review_wrapper.css('.employees-wrapper')
 
         employees = []
 
+        # loops through and stores each employee name
         review_employees.css('a').map do |employee|
           employees.push(employee.text.strip)
         end
+
         review_to_add['employees'] = employees
 
+        # adds other employees if they exist
         if other_employees = review_employees.css('span').css('.italic').text.strip and other_employees.length > 0
           review_to_add['other_employees'] = other_employees
         end
@@ -69,12 +81,13 @@ module TopSecret
         reviews.push(review_to_add)
       end
 
+      # return reviews
       reviews
     end
 
+    # returns correct rating based on class and text of elemnt passed in.
     def self.rating(element)
       return element.text.strip unless element.text.length == 0
-
       rating_re = /\d+/
       return rating_re.match(element.attr('class'))[0]
     end
--- spec/top_secret_spec.rb
+++ spec/top_secret_spec.rb
@@ -1,14 +1,102 @@
 require "spec_helper"
+require 'nokogiri'
+
+chevy_url = "http://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page1/?filter=ALL_REVIEWS#link"
+small_url = "http://www.dealerrater.com/dealer/Jody-Wilkinson-Acura-review-19262/page1/?filter=ALL_REVIEWS"
+large_url = "http://www.dealerrater.com/dealer/Brent-Brown-Toyota-dealer-reviews-11535/page1/?filter=ALL_REVIEWS#link"
+invalid_url = "http://www.dealerrater.com/dealer/Brent-Brown-Toyota-dealer-reviews-11535/#link"
+
+normal_test = TopSecret::Scrape.positive(chevy_url,5,3)
+normal_review = normal_test[0]
 
 RSpec.describe TopSecret do
+
   it "has a version number" do
     expect(TopSecret::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(true).to eq(true)
+  it "has title" do
+    expect(normal_review['title'].length).to be > 0
   end
-  it "returns positive" do
-    expect(TopSecret::Scrape.positive("http://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/#link",5,3)).to eql("Yes!")
+
+  it "has name" do
+    expect(normal_review['name'].length).to be > 0
   end
+
+  it "has date" do
+    expect(normal_review['date'].length).to be > 0
+  end
+
+  it "has overall" do
+    expect(normal_review['overall'].length).to be > 0
+  end
+
+  it "has body" do
+    expect(normal_review['body'].length).to be > 0 
+  end
+
+  it "returns normal size" do
+    expect(normal_test.length).to eql(3)
+  end
+
+  it "returns small size" do
+    expect(TopSecret::Scrape.positive(chevy_url,1,1).length).to eql(1)
+  end
+
+  it "returns large size" do
+    expect(TopSecret::Scrape.positive(chevy_url,10,100).length).to eql(100)
+  end
+
+  it "handles large page number" do
+    expect{TopSecret::Scrape.positive(small_url,10,100)}.to_not raise_error
+  end
+
+  it "handles zero page number" do
+    expect{TopSecret::Scrape.positive(small_url,0,100)}.to_not raise_error
+  end
+
+  it "handles zero limit number" do
+    expect{TopSecret::Scrape.positive(small_url,10,0)}.to_not raise_error
+  end
+
+  it "returns yes rating" do
+    xml = <<EOT
+<mainnode>
+  yes
+</mainnode>
+EOT
+    element = Nokogiri::XML(xml)
+    expect(TopSecret::Scrape.rating(element)).to eql('yes')
+  end
+
+  it "returns no rating" do
+    xml = <<EOT
+<mainnode>
+  no
+</mainnode>
+EOT
+    element = Nokogiri::XML(xml)
+    expect(TopSecret::Scrape.rating(element)).to eql('no')
+  end
+
+#   it "returns 49 rating" do
+#     xml = <<EOT
+# <div>
+# </div>
+# EOT
+#     element = Nokogiri::XML(xml)
+#     element.set_attribute("class","rating-49")
+#     expect(TopSecret::Scrape.rating(element)).to eql('49')
+#   end
+#
+#   it "returns 00 rating" do
+#     xml = <<EOT
+# <div>
+# </div>
+# EOT
+#     element = Nokogiri::XML(xml)
+#     element['class']="rating-static rating-00 margin-top-none pull-right margin-right-none"
+#     expect{TopSecret::Scrape.rating(element)}.to_not raise_error
+#   end
+
 end
