45eb33f05c72ed228e44aa8f40b663ea6941c10a
Josh Black <joshblack@tigger.local>
2017-08-17 12:13:43 -0600

    rubocop

--- lib/top_secret.rb
+++ lib/top_secret.rb
@@ -1,25 +1,24 @@
-require "top_secret/version"
+require 'top_secret/version'
 require 'rest-client'
 require 'nokogiri'
 require 'date'
 require 'pry'
 
 module TopSecret
-
-    class Scrape
-
-    def self.positive(url,pages,limit_to_return)
+  class Scrape
+    # rubocop:disable Metrics/AbcSize
+    def self.positive(url, pages, limit_to_return)
       reviews = []
       counter = 1
       # replaces url and calls extract on correct number of pages starting with the first page.
       while counter <= pages
-        url = url.sub("page" + (counter-1).to_s, "page" + counter.to_s )
+        url = url.sub('page' + (counter - 1).to_s, 'page' + counter.to_s)
         reviews.concat extract(url)
-        counter = counter + 1
+        counter += 1
       end
       # sorts reviews based on teh overall score and then the date.
       reviews.sort { |a, b| [a['overall'], Date.parse(a['date'])] <=> [b['overall'], Date.parse(b['date'])] }
-      return reviews.first(limit_to_return)
+      reviews.first(limit_to_return)
     end
 
     def self.extract(url)
@@ -33,24 +32,24 @@ module TopSecret
 
       # map through each user_review
       noko_page.map do |user_review|
-        review_to_add = Hash.new
+        review_to_add = {}
 
         # the date and overall are located in an earlier div than the rest of the info locates in the review-wrapper container
         date_and_overall = user_review.css('.review-date').css('div')
-        review_to_add["date"] = date_and_overall.css('.italic').text.strip
-        review_to_add["overall"] = rating(date_and_overall.css('.dealership-rating').css('div')[1])
+        review_to_add['date'] = date_and_overall.css('.italic').text.strip
+        review_to_add['overall'] = rating(date_and_overall.css('.dealership-rating').css('div')[1])
 
         # the rest of the review
         review_wrapper = user_review.css('.review-wrapper').css('div')
 
         # title and name
         review_title = review_wrapper[1]
-        review_to_add["title"] = review_title.css('h3').text.strip
-        review_to_add["name"] = review_title.css('span').text.strip
+        review_to_add['title'] = review_title.css('h3').text.strip
+        review_to_add['name'] = review_title.css('span').text.strip
 
         # the main text of the review
         review_body = review_wrapper[2]
-        review_to_add["body"] = review_body.css('p').text.strip
+        review_to_add['body'] = review_body.css('p').text.strip
 
         # the specific ratings
         review_ratings = review_wrapper.css('.review-ratings-all').css('.table').css('.tr')
@@ -74,7 +73,7 @@ module TopSecret
         review_to_add['employees'] = employees
 
         # adds other employees if they exist
-        if other_employees = review_employees.css('span').css('.italic').text.strip and other_employees.length > 0
+        if (other_employees = review_employees.css('span').css('.italic').text.strip) && !other_employees.empty?
           review_to_add['other_employees'] = other_employees
         end
 
@@ -87,10 +86,9 @@ module TopSecret
 
     # returns correct rating based on class and text of elemnt passed in.
     def self.rating(element)
-      return element.text.strip unless element.text.length == 0
+      return element.text.strip unless element.text.empty?
       rating_re = /\d+/
-      return rating_re.match(element.attr('class'))[0]
+      rating_re.match(element.attr('class'))[0]
     end
-
   end
 end
--- top_secret.gemspec
+++ top_secret.gemspec
@@ -1,41 +1,41 @@
 # coding: utf-8
-lib = File.expand_path("../lib", __FILE__)
+
+lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "top_secret/version"
+require 'top_secret/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "top_secret"
+  spec.name          = 'top_secret'
   spec.version       = TopSecret::VERSION
-  spec.authors       = ["Josh Black"]
-  spec.email         = ["josh.black@byu.edu"]
+  spec.authors       = ['Josh Black']
+  spec.email         = ['josh.black@byu.edu']
 
-  spec.summary       = "Podium KGB challenge"
-  spec.description   = "This gem will pull 5 pages of a dealership from DealerRater."
+  spec.summary       = 'Podium KGB challenge'
+  spec.description   = 'This gem will pull 5 pages of a dealership from DealerRater.'
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
-  spec.license       = "MIT"
+  spec.license       = 'MIT'
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against ' \
+      'public gem pushes.'
   end
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
+  spec.files = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.15"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_dependency "nokogiri"
-  spec.add_dependency "rest-client"
-  spec.add_dependency "json"
-  spec.add_dependency "pry"
-
+  spec.add_development_dependency 'bundler', '~> 1.15'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_dependency 'nokogiri'
+  spec.add_dependency 'rest-client'
+  spec.add_dependency 'json'
+  spec.add_dependency 'pry'
 end
